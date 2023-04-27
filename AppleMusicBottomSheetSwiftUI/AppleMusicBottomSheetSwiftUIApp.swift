ea1782e0f0474210becf3f5cf14924b8e6283ffb
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-04-27 23:34:59 +0300

    Initial Commit

---
+++ AppleMusicBottomSheetSwiftUI/AppleMusicBottomSheetSwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  AppleMusicBottomSheetSwiftUIApp.swift
+//  AppleMusicBottomSheetSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 27.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicBottomSheetSwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
